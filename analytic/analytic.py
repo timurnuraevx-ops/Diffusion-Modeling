c29f6f720a837300f6c4ceab3632167ed9adc55f
@timur.nuraev <nuraev.tt@phystech.ru>
2026-04-20 13:49:20 +0300

    Обновил модуль аналитики

--- analytic/analytic.py
+++ analytic/analytic.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import json
 import os
+import subprocess
 
 class DiffusionAnalytic:
     def __init__(self, json_file_name: str, results_file_name: str):
@@ -15,8 +16,8 @@ class DiffusionAnalytic:
         df = pd.read_csv(full_path_res)
         with open(full_path_json, 'r', encoding='utf-8') as f:
             self.config = json.load(f)
-        self.json_file_name = json_file_name
-        self.results_file_name = results_file_name
+        self.json_file = full_path_json
+        self.results_file = full_path_res
 
         self.average_quadratic_radius = np.array(df["r**2"])
         self.t = np.array(df["t * 1e9"])
@@ -54,8 +55,108 @@ class DiffusionAnalytic:
     def GetD(self):
         coeffs = np.polyfit(self.t, self.average_quadratic_radius, 1)
         return coeffs[0] / 6
+    
+    def DiffusionTemperatureGraph(self, T_final: float, n: int, cpp_file_name: str,
+                                json_file_name: str, results_file_name: str, graphic_name: str):
+        folder = "../build"
+        full_path = os.path.join(folder, cpp_file_name)
 
-if __name__ == 'main':
-    diff = DiffusionAnalytic("config.json", "results.csv")
-    diff.TimeRadiusGraph("график 1")
-    print(diff.GetD, diff.TheorD)
+        T = np.linspace(self.T_back, T_final, n)
+        D = []
+        for t in T:
+            self.config["BackgroundGas"]["T"] = t
+        
+            with open(self.json_file, 'w') as f:
+                json.dump(self.config, f, indent=4)
+            
+            result = subprocess.run(
+                [full_path],
+                capture_output=True,
+                text=True
+            )
+            self.__init__(json_file_name, results_file_name)
+            D.append(self.GetD)
+        
+        D = np.array(D)
+        plt.scatter(T, D)
+
+        coeffs = np.polyfit(T, D, 1)
+        x = np.linspace(T[0], T[-1], 100)
+        y = np.polyval(coeffs, x)
+
+        plt.plot(x, y)
+        plt.title("График зависимости коэффициента диффузии от температуры")
+        plt.ylabel("Коэффициент диффузии")
+        plt.xlabel("Температура, К")
+
+        folder = 'graphics/'
+        full_path = os.path.join(folder, graphic_name)
+        plt.savefig(full_path)
+        return coeffs[0]
+    
+    def DiffusionNGraph(self, n_final: str, N: int, cpp_file_name: str,
+                        json_file_name: str, results_file_name: str, graphic_name: str):
+        folder = "../build"
+        full_path = os.path.join(folder, cpp_file_name)
+
+        n = np.linspace(self.n, n_final, N)
+        D = []
+        for i in n:
+            self.config["BackgroundGas"]["n"] = i
+        
+            with open(self.json_file, 'w') as f:
+                json.dump(self.config, f, indent=4)
+            
+            result = subprocess.run(
+                [full_path],
+                capture_output=True,
+                text=True
+            )
+            self.__init__(json_file_name, results_file_name)
+            D.append(self.GetD)
+        
+        D = np.array(D)
+        plt.plot(n, D)
+
+        plt.title("График зависимости коэффициента диффузии от концентрации")
+        plt.ylabel("Коэффициент диффузии")
+        plt.xlabel("Концентрация, м^-3")
+
+        folder = 'graphics/'
+        full_path = os.path.join(folder, graphic_name)
+        plt.savefig(full_path)
+    
+    def DiffusionMuGraph(self, mu_final: str, n: int, cpp_file_name: str,
+                        json_file_name: str, results_file_name: str, graphic_name: str):
+        folder = "../build"
+        full_path = os.path.join(folder, cpp_file_name)
+
+        mu = np.linspace(self.mu_back, mu_final, n)
+        D = []
+        for i in mu:
+            self.config["BackgroundGas"]["mu"] = i
+        
+            with open(self.json_file, 'w') as f:
+                json.dump(self.config, f, indent=4)
+            
+            result = subprocess.run(
+                [full_path],
+                capture_output=True,
+                text=True
+            )
+            self.__init__(json_file_name, results_file_name)
+            D.append(self.GetD)
+        
+        D = np.array(D)
+        plt.plot(mu, D)
+
+        plt.title("График зависимости коэффициента диффузии от молярной массы фонового газа")
+        plt.ylabel("Коэффициент диффузии")
+        plt.xlabel("Молярная масса, кг/моль")
+
+        folder = 'graphics/'
+        full_path = os.path.join(folder, graphic_name)
+        plt.savefig(full_path)
+
+diff = DiffusionAnalytic("config.json", "results.csv")
+print(diff.DiffusionMuGraph(0.5, 11, "main", "config.json", "results.csv", "график диффузии от молярной массы"))
